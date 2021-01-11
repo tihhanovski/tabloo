f07268fb027e17a34fa5499280c963d726d0c362
Ilja Tihhanovski <ilja.tihhanovski@gmail.com>
2021-01-11 08:59:17 +0000

    #1 cleanup

--- server/web/setup.dist.php
+++ server/web/setup.dist.php
@@ -3,6 +3,3 @@
 const DB_PWD = "...put your mysql password here...";
 const DB_UID = "...put your mysql user here...";
 const DB_DB = "...put your mysql db name here...";
-
-const GTFS_SERVER = "http://peatus.ee/gtfs/";
-const GTFS_FILE = "gtfs.zip";
