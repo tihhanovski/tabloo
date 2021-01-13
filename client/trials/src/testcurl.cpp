c486b6ed9c0eb1da7e27fbce6f6afe06fb31da2c
Ilja Tihhanovski <ilja.tihhanovski@gmail.com>
2021-01-13 14:20:57 +0000

    #2

--- client/trials/src/testcurl.cpp
+++ client/trials/src/testcurl.cpp
@@ -6,12 +6,37 @@
 //code from here
 //https://stackoverflow.com/questions/44994203/how-to-get-the-http-response-string-using-curl-in-c/45017565
 
+//Compile it with -lcurl:
+//g++ src/testcurl.cpp -o bin/testcurl -Wall -lcurl
+
 using namespace std;
 
 size_t writeCallback(char *contents, size_t size, size_t nmemb, void *userp)
 {
     ((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
+}
+
+char* getDataFromInternet()
+{
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    CURL* easyhandle = curl_easy_init();
+    std::string readBuffer;
+
+    curl_easy_setopt(easyhandle, CURLOPT_URL, "https://dev.intellisoft.ee/tabloo/ask/?c=7820162-1");
+    //curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1L);
+    //curl_easy_setopt(easyhandle, CURLOPT_PROXY, "http://my.proxy.net");   // replace with your actual proxy
+    //curl_easy_setopt(easyhandle, CURLOPT_PROXYPORT, 8080L);
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
+
+    curl_easy_perform(easyhandle);
+
+    size_t len = readBuffer.length();
+	char* data = new char[len + 1];
+	readBuffer.copy(data, len, 0);
+    return data;
 }
 
 class LineData
@@ -36,25 +61,7 @@ struct TimeData
 
 int main(int argc, char * argv[])
 {
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    CURL* easyhandle = curl_easy_init();
-    std::string readBuffer;
-
-    curl_easy_setopt(easyhandle, CURLOPT_URL, "https://dev.intellisoft.ee/tabloo/ask/?c=7820162-1");
-    //curl_easy_setopt(easyhandle, CURLOPT_VERBOSE, 1L);
-    //curl_easy_setopt(easyhandle, CURLOPT_PROXY, "http://my.proxy.net");   // replace with your actual proxy
-    //curl_easy_setopt(easyhandle, CURLOPT_PROXYPORT, 8080L);
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, writeCallback);
-    curl_easy_setopt(easyhandle, CURLOPT_WRITEDATA, &readBuffer);
-
-    curl_easy_perform(easyhandle);
-
-    //std::cout << readBuffer << std::endl;
-
-    size_t len = readBuffer.length();
-	char* data = new char[len + 1];
-	readBuffer.copy(data, len, 0);
+    char* data = getDataFromInternet();
 
     unsigned char lineCount = *data;
 
@@ -92,9 +99,6 @@ int main(int argc, char * argv[])
         cout << (int)times[i].hours << ":" << (int)times[i].minutes << " "
         << times[i].line->shortName << " " << times[i].line->longName << " -> " << times[i].line->headSign
         << endl;
-
-
-
 
     return 0;
 }
